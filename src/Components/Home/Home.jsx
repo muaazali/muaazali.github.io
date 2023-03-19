a1b4dfe429270f601b8286287da15b4448b9983c
Muaaz Ali <muaazali512@gmail.com>
2023-03-19 16:36:38 +0500

    Updated home page with picture and content.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,47 @@
 import React from "react";
+import {
+	SiUnity,
+	SiReact,
+	SiAngular,
+	SiJavascript,
+	SiCsharp,
+	SiPython,
+	SiAmazonaws,
+} from "react-icons/si";
 
 export default function Home() {
 	return (
 		<div className="tw-w-full tw-h-full tw-flex tw-justify-center tw-items-center tw-flex-col">
-			<div className="md:tw-flex md:tw-justify-around md:tw-w-full md:tw-flex-row-reverse">
-				<div>Image Here</div>
-				<div>Details Here</div>
+			<div className="md:tw-flex md:tw-justify-around md:tw-items-center md:tw-w-full md:tw-flex-row-reverse">
+				<div>
+					<img
+						src="images/home-pic.jpg"
+						className="tw-w-64 tw-h-64 tw-rounded-full"
+					></img>
+				</div>
+				<div>
+					<div className="tw-text-2xl tw-font-extralight">
+						<span>I'm a </span>
+						<span className="tw-font-bold tw-text-3xl tw-text-sky-400">
+							Software Engineer
+						</span>
+					</div>
+					<div className="tw-text-3xl tw-py-2">
+						<div className="tw-flex tw-justify-center seperate-icons md:tw-justify-start">
+							<SiUnity></SiUnity>
+							<SiCsharp></SiCsharp>
+							{/* </div>
+						<div className="tw-flex seperate-icons tw-mt-2"> */}
+							<SiReact></SiReact>
+							<SiAngular></SiAngular>
+							<SiJavascript></SiJavascript>
+							<SiPython></SiPython>
+							<SiAmazonaws></SiAmazonaws>
+						</div>
+					</div>
+					<div>Portfolio stuff here.</div>
+				</div>
 			</div>
-			<div>Other content here.</div>
 		</div>
 	);
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,17 +7,17 @@ export default function Navbar() {
 			className="tw-p-2 tw-flex tw-justify-around tw-items-center tw-w-full"
 			style={{ position: "absolute", top: "0" }}
 		>
-			<div className="tw-hidden md:tw-block tw-font-bold tw-text-2xl hover:tw-text-blue-900">
+			<div className="tw-hidden md:tw-block tw-font-bold tw-text-2xl tw-text-sky-400 hover:tw-text-blue-900">
 				<Link to={"/"}>Syed Muaaz Ali</Link>
 			</div>
 			<div className="tw-w-full md:tw-w-1/2 lg:tw-w-1/3 tw-flex tw-text-center tw-justify-around tw-font-light tw-uppercase">
-				<span className="hover:tw-text-blue-900">
+				<span className="hover:tw-text-sky-400">
 					<Link to={"/projects"}>Projects</Link>
 				</span>
-				<span className="hover:tw-text-blue-900">
+				<span className="hover:tw-text-sky-400">
 					<Link to={"/about"}>About</Link>
 				</span>
-				<span className="hover:tw-text-blue-900">
+				<span className="hover:tw-text-sky-400">
 					<Link to={"/contacts"}>Contacts</Link>
 				</span>
 			</div>
