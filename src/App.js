1a63822c91e867910ce802b2d81f7e333e8a655d
Muaaz Ali <muaazali512@gmail.com>
2023-03-19 15:20:12 +0500

    Set up a basic layout.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import logo from './logo.svg';
 import "./App.scss";
 
 function App() {
-	return <div className="App">Hello world!</div>;
+	return (
+		<div className="tw-w-screen tw-h-screen tw-flex tw-justify-center tw-items-center tw-flex-col">
+			<div className="md:tw-flex md:tw-justify-around md:tw-w-full md:tw-flex-row-reverse">
+				<div>Image Here</div>
+				<div>Details Here</div>
+			</div>
+			<div>Other content here.</div>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import "./index.scss";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App></App>,
+		errorElement: <ErrorPage />,
+	},
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
